ad0ddfe55884280d8cc0407e764f20f91d89d6eb
Renan Aizawa Alves Mascarenhas <renanaizawa@outlook.com>
2022-10-14 12:28:18 -0400

    feat: correção no service do nome da model, permitiu concluir os req 8 e 9

--- src/services/Category.js
+++ src/services/Category.js
@@ -1,15 +1,15 @@
-const { Caterogy } = require('../models');
+const { Category } = require('../models');
 
 const createCAt = async (name) => {
     if (!name) return { code: 400, message: '"name" is required' };
     console.log('name no service cat:', name);
-    const data = await Caterogy.create({ name });
+    const data = await Category.create({ name });
     console.log('create category service:', data);
     return data;
 };
 
 const allCat = async () => {
-    const data = await Caterogy.findAll();
+    const data = await Category.findAll();
     return data;
 };
 
